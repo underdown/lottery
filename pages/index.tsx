295c16b7b89c9903ce2abfc7be1ef561e69315d5
Ryan Underdown <ryanunderdown@gmail.com>
2024-03-29 19:45:50 -0700

    hp text

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <meta name='description' content='A Starter for styled-components' />
         <link rel='icon' href='/wave.ico' />
       </Head>
-      <Title>This is my styled component!</Title>
+      <Title>The Lottery</Title>
     </div>
   );
 }

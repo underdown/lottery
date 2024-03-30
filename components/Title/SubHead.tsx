3a944d67c767a1310fc05d6a2ff840c4a4d9f5f2
Ryan Underdown <ryanunderdown@gmail.com>
2024-03-29 21:41:04 -0700

    margins

--- components/Title/SubHead.tsx
+++ components/Title/SubHead.tsx
@@ -4,11 +4,11 @@ const SubHead = styled.h2`
   font-size: 0.2rem;
   color: red;
   text-align: center;
-  margin-top: 5vh;
+  margin-top: 1vh;
   font-family: "Courier New", monospace;
   white-space: pre;
   font-style: normal;
-  background-image: -webkit-linear-gradient(180deg, #c0c0c0, #fff);
+  background-image: -webkit-linear-gradient(45deg, #c0c0c0, #fff);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
 
